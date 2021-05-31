e9026beb5967c83465d1bb6f5b9da257ed8ee151
kiuy2 <kiuy2@naver.com>
2021-06-01 00:55:27 +0900

    add cheerleader stand animation

--- scripts/render.js
+++ scripts/render.js
@@ -1,16 +1,14 @@
-const cheer_ani = [];
-const dancer_ani = [];
+const dancer_ani = new Array(5);
+const cheer_head_ani = new Array(2);
+const cheer_body_ani = [ new Array(5), new Array(5) ];
 var prev_frame = new Array(3);
-var cheer_idx = 0;
 var stand_idx = 0;
-
-var cheer_info;
-const cheer_head = [ { "x": 29, "y": 40 }, { "x": 27, "y": 40 } ];
+var cheer_id = 0;
+var cheer_idx = 0;
 
 $(document).ready(function() {
     const objects = document.getElementById("objects");
     const NPC = document.createElement("img");
-    const cheer0 = document.createElement("img");
     const cheerleader = [];
     const dancer = document.createElement("div");
 
@@ -22,56 +20,52 @@ $(document).ready(function() {
     NPC.src = "images/9062540.gif";
     objects.appendChild(NPC);
 
-    const requestURL = 'https://github.com/kiuy2/maplestory_blooming_dance_party/blob/main/data/springDance.json';
-    const request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function() {
-        cheer_info = request.response;
-        console.log(cheer_info[0][0][0].origin.x);
-        console.log(cheer_info[0][0][0].origin.y);
-        console.log(cheer_info[0][0][0].neck.x);
-        console.log(cheer_info[0][0][0].neck.y);
-    }
-
-    // cheer0
-    cheer0.id = "cheer_0";
-    cheer0.style.position = "absolute";
-    cheer0.style.top = "521px";
-    cheer0.style.left = "226px";
-    cheer0.src = "images/cheer0.gif";
-    objects.appendChild(cheer0);
-    // // cheerleaders
-    // cheerleader.push(document.createElement("div"));
-    // cheerleader[0].id = "cheerleader_0";
-    // cheerleader[0].style.position = "absolute";
-    // cheerleader[0].style.top = "530px";
-    // cheerleader[0].style.left = "240px";
-    // objects.appendChild(cheerleader[0]);
-    // cheerleader.push(document.createElement("div"));
-    // cheerleader[1].id = "cheerleader_1";
-    // cheerleader[1].style.position = "absolute";
-    // cheerleader[1].style.top = "530px";
-    // cheerleader[1].style.left = "1040px";
-    // objects.appendChild(cheerleader[1]);
-    // dancer
+    // cheerleaders
+    cheerleader.push(document.createElement("div"));
+    cheerleader[0].id = "cheerleader_0";
+    cheerleader[0].style.position = "absolute";
+    cheerleader[0].style.top = "620px";
+    cheerleader[0].style.left = "440px";
+    objects.appendChild(cheerleader[0]);
+    cheerleader.push(document.createElement("div"));
+    cheerleader[1].id = "cheerleader_1";
+    cheerleader[1].style.position = "absolute";
+    cheerleader[1].style.top = "620px";
+    cheerleader[1].style.left = "1040px";
+    objects.appendChild(cheerleader[1]);
+    dancer
     dancer.id = "dancer";
     dancer.style.position = "absolute";
     dancer.style.top = "390px";
     dancer.style.left = "545px";
     objects.appendChild(dancer);
 
-    // // cheer
-    // for (var i=0; i<1; i++) {
-    //     cheer_ani[i] = new Array();
-    //     for (var j=0; j<48; j++) {
-    //         cheer_ani[i].push(document.createElement("img"));
-    //         cheer_ani[i][j].src = "images/cheer"+i+"/"+j+".png";
-    //         cheer_ani[i][j].style.display = "none";
-    //         cheerleader[i].appendChild(cheer_ani[i][j]);
-    //     }
-    // }
+    // cheer head
+
+    // cheer (head, stand)
+    cheer_body_ani[0][0] = new Array();
+    cheer_body_ani[1][0] = new Array();
+    for (var i=0; i<2; i++) {
+        cheer_head_ani[i] = new Object();
+        for (var j=0; j<5; j++) {
+            cheer_head_ani[i][emotions[j]] = document.createElement("img");
+            cheer_head_ani[i][emotions[j]].src = "images/springDance/head_"+i+"/"+emotions[j]+".png";
+            cheer_head_ani[i][emotions[j]].style.display = "none";
+            cheer_head_ani[i][emotions[j]].style.position = "absolute";
+            cheerleader[i].appendChild(cheer_head_ani[i][emotions[j]]);
+        }
+
+        cheer_body_ani[i][0] = new Array();
+        for (var j=0; j<4; j++) {
+            cheer_body_ani[i][0].push(document.createElement("img"));
+            cheer_body_ani[i][0][j].src = "images/springDance/body_"+i+"_0.frames/"+String(j*180)+".png";
+            cheer_body_ani[i][0][j].style.display = "none";
+            cheer_body_ani[i][0][j].style.position = "absolute";
+            cheer_body_ani[i][0][j].style.left = -cheer_body[i][0][j].origin.x;
+            cheer_body_ani[i][0][j].style.top = -cheer_body[i][0][j].origin.y;
+            cheerleader[i].appendChild(cheer_body_ani[i][0][j]);
+        }
+    }
     // dance (stand)
     dancer_ani[0] = new Array();
     for (var i=0; i<6; i++) {
@@ -91,17 +85,19 @@ $(document).ready(function() {
         }
     }
 
-    //setInterval(cheerlead, 90);
     dancer_ani[0][0].style.display = "block";
+    cheer_head_ani[0]["default"].style.display = "block";
+    cheer_head_ani[1]["default"].style.display = "block";
+    cheer_body_ani[0][0][0].style.display = "block";
+    cheer_body_ani[1][0][0].style.display = "block";
     prev_frame[0] = dancer_ani[0][0];
+    prev_frame[1] = cheer_head_ani[0]["default"]
+    prev_frame[2] = cheer_head_ani[1]["default"]
+    prev_frame[3] = cheer_body_ani[0][0][0];
+    prev_frame[4] = cheer_body_ani[1][0][0];
+    cheer_id = setInterval(cheer_stand, 180);
     dance_stand();
 });
-
-// function cheerlead() {
-//     cheer_ani[0][cheer_idx].style.display = "none";
-//     cheer_idx = (cheer_idx + 1) % 48;
-//     cheer_ani[0][cheer_idx].style.display = "block";
-// }
 
 function dance_command() {
     prev_frame[0].style.display = "none";
@@ -120,6 +116,24 @@ function dance_command() {
         setTimeout(dance, i*full_delay, each_delay, dir, 0);
     }
     setTimeout(dance_stand, length*full_delay);
+}
+
+function cheer_stand() {
+    var emo = 'default';
+    for (var i=1; i<5; i++) prev_frame[i].style.display = "none";
+    cheer_head_ani[0][emo].style.left = cheer_body[0][0][cheer_idx].neck.x - cheer_head[0].x;
+    cheer_head_ani[0][emo].style.top = cheer_body[0][0][cheer_idx].neck.y - cheer_head[0].y;
+    cheer_head_ani[0][emo].style.display = "block";
+    cheer_head_ani[1][emo].style.left = cheer_body[1][0][cheer_idx].neck.x - cheer_head[1].x;
+    cheer_head_ani[1][emo].style.top = cheer_body[1][0][cheer_idx].neck.y - cheer_head[1].y;
+    cheer_head_ani[1][emo].style.display = "block";
+    cheer_body_ani[0][0][cheer_idx].style.display = "block";
+    cheer_body_ani[1][0][cheer_idx].style.display = "block";
+    prev_frame[1] = cheer_head_ani[0][emo];
+    prev_frame[2] = cheer_head_ani[1][emo];
+    prev_frame[3] = cheer_body_ani[0][0][cheer_idx];
+    prev_frame[4] = cheer_body_ani[1][0][cheer_idx];
+    cheer_idx = (cheer_idx + 1) % 4;
 }
 
 function dance_stand() {
---
+++ scripts/springDance.js
@@ -0,0 +1,53 @@
+const cheer_head = [ { x: 34, y: 39 }, {x: 27, y: 39 } ];
+const emotions = [ "default", "blink_0", "blink_1", "wink", "chu" ];
+
+const cheer_body = [
+    // [0] left side
+    [
+        // [0][0] stand
+        [
+            {
+                origin: { x: 14, y: 30 },
+                neck: { x: -2, y: -32 } 
+            },
+            {
+                origin: { x: 14, y: 29 },
+                neck: { x: -3, y: -31 }
+            },
+            {
+                origin: { x: 14, y: 30 },
+                neck: { x: -3, y: -30 }
+            },
+            {
+                origin: { x: 14, y: 29 },
+                neck: { x: -3, y: -31 }
+            }
+        ],
+        // [0][1] left
+        [
+
+        ]
+    ],
+    // [1] right side
+    [
+        // [1][0] stand
+        [
+            {
+                origin: { x: 12, y: 30 },
+                neck: { x: 3, y: -32 }
+            },
+            {
+                origin: { x: 12, y: 29 },
+                neck: { x: 4, y: -31 }
+            },
+            {
+                origin: { x: 12, y: 30 },
+                neck: { x: 4, y: -30 }
+            },
+            {
+                origin: { x: 12, y: 29 },
+                neck: { x: 4, y: -31 }
+            }
+        ]
+    ]
+]
